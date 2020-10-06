3564ad4969d231d1e2a41e9c86c8ad55f05486cd
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-10-06 11:25:40 +0200

    streamline setup and CMakeLists

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 import os
-import re
 import sys
-import platform
 import subprocess
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
-
-
-USE_OPENBLAS = os.environ.get('USE_OPENBLAS', False)
 
 
 class CMakeExtension(Extension):
@@ -21,33 +15,22 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            subprocess.check_output(['cmake', '--version'])
         except OSError:
             raise RuntimeError("CMake must be installed to build the following extensions: " +
                                ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        cmake_args = ['-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DUSE_OPENBLAS=' + str(USE_OPENBLAS)]
+        cmake_args = ['-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
-        if platform.system() == "Windows":
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j4']
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+        build_args += ['--', '-j4']
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
@@ -64,7 +47,8 @@ setup(
     author='Hany Abdulsamad',
     author_email='hany@robot-learning.de',
     description='A toolbox for trajectory optimization',
-    long_description='',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'scikit-learn',
+                      'autograd', 'gym', 'pathos'],
     ext_modules=[CMakeExtension('gps', './trajopt/gps/'),
                  CMakeExtension('ilqr', './trajopt/ilqr/'),
                  CMakeExtension('bspilqr', './trajopt/bspilqr/')],
