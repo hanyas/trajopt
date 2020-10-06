71b48e4c1fdd6cbf8610a6202f1e9bbd24c31b08
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-10-06 13:02:40 +0200

    update lqr env

--- trajopt/envs/lqr/lqr.py
+++ trajopt/envs/lqr/lqr.py
@@ -12,26 +12,25 @@ class LQR(gym.Env):
         self.dm_act = 1
 
         self._dt = 0.1
+
         self._g = np.array([10., 10.])
+        self._gw = np.array([1e1, 1e1])
+
+        self._uw = np.array([1.])
+        self._umax = np.inf
+        self.action_space = spaces.Box(low=-self._umax,
+                                       high=self._umax, shape=(1,))
+
+        self._xmax = np.array([np.inf, np.inf])
+        self.observation_space = spaces.Box(low=-self._xmax,
+                                            high=self._xmax)
 
         # stochastic dynamics
         self._A = np.array([[1.1, 0.], [1.0, 1.0]])
         self._B = np.array([[1.], [0.]])
         self._c = - self._A @ self._g  # stable at goal
 
-        self._sigma = 1e-8 * np.eye(2)
-
-        self._gw = np.array([1e1, 1e1])
-        self._uw = np.array([1.])
-
-        self._xmax = np.array([np.inf, np.inf])
-        self._umax = np.inf
-
-        self.action_space = spaces.Box(low=-self._umax,
-                                       high=self._umax, shape=(1,))
-
-        self.observation_space = spaces.Box(low=-self._xmax,
-                                            high=self._xmax)
+        self._sigma = 1e-8 * np.eye(self.dm_state)
 
         self.state = None
         self.np_random = None
@@ -55,7 +54,7 @@ class LQR(gym.Env):
         return self._g
 
     def dynamics(self, x, u):
-        _u = np.clip(u, -self._umax, self._umax)
+        _u = np.clip(u, -self.ulim, self.ulim)
 
         def f(x, u):
             return self._A @ x + self._B @ u + self._c
@@ -66,12 +65,12 @@ class LQR(gym.Env):
         k4 = f(x + self.dt * k3, _u)
 
         xn = x + self.dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4)
-        xn = np.clip(xn, -self._xmax, self._xmax)
+        xn = np.clip(xn, -self.xlim, self.xlim)
 
         return xn
 
     def inverse_dynamics(self, x, u):
-        _u = np.clip(u, -self._umax, self._umax)
+        _u = np.clip(u, -self.ulim, self.ulim)
 
         def f(x, u):
             return self._A @ x + self._B @ u + self._c
@@ -82,17 +81,18 @@ class LQR(gym.Env):
         k4 = f(x - self.dt * k3, _u)
 
         xn = x - self.dt / 6. * (k1 + 2. * k2 + 2. * k3 + k4)
-        xn = np.clip(xn, -self._xmax, self._xmax)
+        xn = np.clip(xn, -self.xlim, self.xlim)
 
         return xn
 
     def noise(self, x=None, u=None):
-        _u = np.clip(u, -self._umax, self._umax)
-        _x = np.clip(x, -self._xmax, self._xmax)
+        _u = np.clip(u, -self.ulim, self.ulim)
+        _x = np.clip(x, -self.xlim, self.xlim)
         return self._sigma
 
-    def cost(self, x, u, a):
-        return a * (x - self._g).T @ np.diag(self._gw) @ (x - self._g) + u.T @ np.diag(self._uw) @ u
+    def cost(self, x, u):
+        return self.dt * ((x - self._g).T @ np.diag(self._gw) @ (x - self._g)
+                          + u.T @ np.diag(self._uw) @ u)
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
