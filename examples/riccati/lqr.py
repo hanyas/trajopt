47a58ebc068801b8b51e8146c466ceefc51bbc83
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2020-10-06 13:02:50 +0200

    update lqr example

--- examples/riccati/lqr.py
+++ examples/riccati/lqr.py
@@ -1,3 +1,5 @@
+import autograd.numpy as np
+
 import gym
 from trajopt.riccati import Riccati
 
@@ -5,7 +7,19 @@ from trajopt.riccati import Riccati
 env = gym.make('LQR-TO-v0')
 env._max_episode_steps = 100
 
-alg = Riccati(env, nb_steps=60)
+dm_state = env.observation_space.shape[0]
+dm_act = env.action_space.shape[0]
+
+nb_steps = 60
+
+env_sigma = env.unwrapped._sigma
+
+state = np.zeros((dm_state, nb_steps + 1))
+action = np.zeros((dm_act, nb_steps))
+
+state[:, 0] = env.reset()
+alg = Riccati(env, nb_steps=nb_steps,
+              init_state=tuple([state[:, 0], env_sigma]))
 
 # run iLQR
 trace = alg.run()
