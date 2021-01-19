985678adc520764835da04e9e4592d3712eb3624
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2021-01-19 14:58:21 +0100

    update rgps examples

--- examples/rgps/mf_pendulum.py
+++ examples/rgps/mf_pendulum.py
@@ -8,21 +8,23 @@ warnings.filterwarnings("ignore")
 
 # np.random.seed(1337)
 
-# lqr task
-env = gym.make('LQR-TO-v0')
-env._max_episode_steps = 100000
+# pendulum task
+env = gym.make('Pendulum-TO-v0')
+env._max_episode_steps = 100
+env.unwrapped.dt = 0.05
 
 prior = {'K': 1e-6}
 
-alg = MFRGPS(env, nb_steps=60,
-             policy_kl_bound=0.5,
+alg = MFRGPS(env, nb_steps=100,
+             policy_kl_bound=1.,
              param_kl_bound=10.,
              init_state=env.init(),
-             init_action_sigma=100.,
+             init_action_sigma=1.,
+             action_penalty=np.array([1e-5]),
+             slew_rate=False,
              prior=prior)
 
-trace = alg.run(nb_episodes=25, nb_iter=25,
-                verbose=True, debug_dual=False)
+trace = alg.run(nb_episodes=50, nb_iter=25, verbose=True)
 
 alg.plot()
 
@@ -37,7 +39,7 @@ for k in range(alg.dm_state):
 
 for k in range(alg.dm_act):
     plt.subplot(alg.dm_state + alg.dm_act, 1, alg.dm_state + k + 1)
-    plt.plot(data['u'][k, ...])
+    plt.plot(np.clip(data['u'][k, ...], - env.ulim, env.ulim))
 
 plt.show()
 
