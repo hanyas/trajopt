3c412f305e07b700452b4aca656d760b526bc842
Hany Abdulsamad <hany@robot-learning.de>
2022-06-16 10:00:35 +0300

    add clean script

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+rm trajopt/bspilqr/*.so
+rm -r trajopt/bspilqr/build
+
+rm trajopt/ilqr/*.so
+rm -r trajopt/ilqr/build
+
+rm trajopt/gps/*.so
+rm -r trajopt/gps/build
+
+rm trajopt/rgps/*.so
+rm -r trajopt/rgps/build
